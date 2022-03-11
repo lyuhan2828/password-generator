849458d90ed161a93c5ff880a4afb0bd0d2da56d
Yu Han <yuhan28283@gmail.com>
2022-03-11 13:21:44 -0500

    update

--- script.js
+++ script.js
@@ -1,34 +1,68 @@
 // Assignment code here
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-var passwLength = (prompt("Please choose between 8 to 128 legnth of characters for password."));
-var passwordLength = x < 128;
-var passwordLength = x > 8;
-var password = x
-
-var confirmLowercase;
-var confirmUppercase;
-var confirmNumber;
-var confirmCharacters;
-
 var alphabetLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var alphabetUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var characters = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "+", "=", "_", "<", ">", "/", "?"];
 
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+function generatePassword(){
+var passwLength = prompt("Please choose between 8 to 128 legnth of characters for password.");
+if (!passwLength) {
+  alert("Please pick a password length")
+  return null
+}
+if (passwLength < 8 || passwLength > 128) {
+  alert("Please pick a password between 8 and 128")
+  return null
+}
+
+var confirmLowercase = confirm("Do you want lowercase") ;
+var confirmUppercase = confirm("Do you want uppercase");
+var confirmNumber = confirm("Do you want numbers");
+var confirmCharacters = confirm("Do you want characters");
+if (!confirmLowercase && !confirmUppercase && !confirmNumber && !confirmCharacters) {
+  alert("Please choose at least one character type")
+  return null
+}
+var chosenCharacters = [];
+
+
+if (confirmLowercase) {
+chosenCharacters = chosenCharacters.concat(alphabetLower)
+}
+if (confirmUppercase) {
+  chosenCharacters = chosenCharacters.concat(alphabetUpper)
+}
+if (confirmNumber) {
+  chosenCharacters = chosenCharacters.concat(number)
+  }
+if (confirmCharacters) {
+    chosenCharacters = chosenCharacters.concat(characters)
+    }
+    var finalPass = []
+    for (let i = 0; i < passwLength; i++) {
+      const element = randomElement(chosenCharacters)
+      finalPass.push(element)
+    }
+    console.log(finalPass)
+    return finalPass.join("")
+}
+
+function randomElement(array) {
+  const randIndex = Math.floor(Math.random * array.length);
+  const randomItem = array[randIndex]
+  return randomItem;
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  
+  console.log(password)
   var passwordText = document.querySelector("#password");
-  if (x > 8 &&  x < 128) {
-    password = "invalid";
-  } else {
-    (x < 8 &&  x > 128) {
-      password = "valid";
-    }
+  
 
   passwordText.value = password;
 
